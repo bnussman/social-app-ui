1bb36bf3ee9e46e6eb9fc693de14a6878faf5548
Banks Nussman <banks@nussman.us>
2021-10-25 23:38:58 -0400

    fix

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,3 @@
-export const API_URL = "https://social-app.bnussman.workers.dev";
+export const API_URL = "https://proxy.nussman.us";
+//export const API_URL = "https://social-app.bnussman.workers.dev";
 // export const API_URL = "http://localhost:8787";
